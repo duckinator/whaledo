d7b67e9cfdd18e4844f02e33f4c8363a56ef2ff5
Ellen Marie Dash <me@duckie.co>
2017-11-27 14:36:12 -0500

    ????

--- setup.py
+++ setup.py
@@ -3,11 +3,25 @@ from distutils.core import setup
 setup(
     name='whaledo',
     version='1.0',
-    description='A thing for working with Docker containers designed for isolating interactive tools.',
+    description='A program for working with Docker containers designed for isolating interactive tools.',
     author='Ellen Marie Dash',
     author_email='me@duckie.co',
     url='https://github.com/duckinator/whaledo',
     license='MIT',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
     entry_points={
         'console_scripts': [
             'whaledo',
