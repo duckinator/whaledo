7481e47910a91b6ad990e13359445ea015d113c2
Ellen Marie Dash <me@duckie.co>
2017-11-27 15:45:29 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='whaledo',
-    version='1.0.0',
+    version='1.0.1',
     description='A program for working with Docker containers designed for isolating interactive tools.',
     author='Ellen Marie Dash',
     author_email='me@duckie.co',
